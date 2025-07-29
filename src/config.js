91b2f24d76283a52b8856ae572163ce294d0237b
Your Name <info@heroz.sa>
2025-07-29 20:43:29 +0300

    message for commit work

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL= 'http://localhost:3000/api';
-export const API_BASE_URL_remote = 'http://3.28.61.89:3000/api';
+export const API_BASE_URL_local= 'http://localhost:3000/api';
+export const API_BASE_URL = 'http://3.28.61.89:3000/api';
