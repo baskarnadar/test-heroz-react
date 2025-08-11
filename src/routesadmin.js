22cbddb8ca0252f5643b2264a377e837693e6c45
Your Name <info@heroz.sa>
2025-08-11 13:45:44 +0300

    message for commit work

--- src/routesadmin.js
+++ src/routesadmin.js
@@ -291,7 +291,7 @@ const routes = [
   { path: '/mainmenu/modify', name: 'banner List', element: MainMenuModify }, 
   { path: '/rights/list', name: 'banner List', element: RightsList },
   
-  { path: '/public/program/:requestId', name: 'Program', element: PublicProposal },
+{ path: '/public/proposal', name: 'Proposal', element: PublicProposal },
 
   { path: '/public/payerror', name: 'Proposal ', element: PublicPayError },
   { path: '/public/paysuccess', name: 'Proposal ', element: PublicPaySuccess },
