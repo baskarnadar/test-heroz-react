e1d248df0ae36c84a838ea18358c2306b5aa5f5a
Your Name <info@heroz.sa>
2025-09-24 06:35:35 +0300

    school changes

--- src/routesadmin.js
+++ src/routesadmin.js
@@ -3,7 +3,7 @@ const AdminDashboard = React.lazy(() => import('./admindata/dashboard/Dashboard'
 const LoginPage = React.lazy(() => import('./views/pages/login/login.js'))
 const PublicProposal = React.lazy(() => import('./public/proposal.js'))
 const PublicProgram = React.lazy(() => import('./public/program.js'))
-const PublicProgramv1 = React.lazy(() => import('./public/programv1.js'))
+ 
 const PublicPayError = React.lazy(() => import('./public/payerror.js'))
 const PublicPaySuccess = React.lazy(() => import('./public/paysuccess.js'))
 
@@ -299,7 +299,7 @@ const routes = [
   
 { path: '/public/proposal', name: 'Proposal', element: PublicProposal },
 { path: '/public/program/:requestId', name: 'Program', element: PublicProgram },
-{ path: '/public/programv1/:requestId', name: 'Program', element: PublicProgramv1 },
+ 
   { path: '/public/payerror', name: 'Proposal ', element: PublicPayError },
   { path: '/public/paysuccess', name: 'Proposal ', element: PublicPaySuccess },
 
