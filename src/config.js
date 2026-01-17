a61ac0c984ec6089da52f40892fbaef54463ac90
baskarnadar <baskarnadar@gmail.com>
2026-01-17 17:21:08 +0300

    API

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 //------------------------TEST SERVER
-export const API_BASE_URL ='http://3.28.121.180:3000/api'; // Test server
+export const API_BASE_URL ='https://testapi.heroz.sa/api'; // Test server
 //export const API_BASE_URL ='http://localhost:3000/api'; // Test server
-export const BaseURL ='http://40.172.154.153:3000/login';
+export const BaseURL ='https://test.heroz.sa/login';
  
